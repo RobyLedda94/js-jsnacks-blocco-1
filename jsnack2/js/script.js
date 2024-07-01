04b5e5e9dc880abfe48278f1fa3990d591f4a36b
RobyLedda94 <robyledda01@yahoo.it>
2024-07-01 15:26:55 +0200

    [UPD] ok messaggio di debug snack2

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,3 +1,8 @@
 // SNACK 2:
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell'array
+
+
+// messaggio di debug
+
+// alert('riempiamo un array con i valori');
