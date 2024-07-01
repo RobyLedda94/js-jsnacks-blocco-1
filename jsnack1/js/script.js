a6ad56fb8a7d954e883aebc03467b902b55a0cbb
RobyLedda94 <robyledda01@yahoo.it>
2024-07-01 15:09:24 +0200

    [UPD]snack1 add array

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,5 +2,11 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 
-// messaggio di debug
-alert('Questa e una lista di invitati')
+// // messaggio di debug
+// alert('Questa e una lista di invitati')
+
+
+// Creazione array con lista nomi degli invitati
+
+const usersName = ['antonio', 'gianluca', 'roberta', 'roberto', 'alessandro'];
+console.log(usersName);
